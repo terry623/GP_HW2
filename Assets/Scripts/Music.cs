f0033102d0f2425b14816a529a8ad5f9782a6ea0
terry623 <terry92516@gmail.com>
2018-11-08 20:55:36 +0800

    add background music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
