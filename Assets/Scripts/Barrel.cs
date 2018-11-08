856d5492b3cd5926c422eb59ef996da1f8ce8423
terry623 <terry92516@gmail.com>
2018-11-08 11:18:01 +0800

    fall barrel

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    private float windForce = 20.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("MovedByWind", 0.0f, 0.2f);
+    }
+
+    void MovedByWind()
+    {
+        if (Random.Range(0.0f, 1.0f) > 0.5f) GetComponent<Rigidbody>().AddForce(Vector3.right * windForce);
+        else GetComponent<Rigidbody>().AddForce(-1 * Vector3.right * windForce);
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime * 100.0f, Space.World);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "env") Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float velocity = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.back * velocity);
+        else if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.right * velocity);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.forward * velocity);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.left * velocity);
+    }
+}
