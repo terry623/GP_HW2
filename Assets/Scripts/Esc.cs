ea50362837c960d8e91401da57a807b740f26f06
terry623 <terry92516@gmail.com>
2018-11-08 21:37:13 +0800

    add escape page

---
+++ Assets/Scripts/Esc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esc : MonoBehaviour
+{
+    public Canvas escCanvas;
+    public Canvas scoreCanvas;
+    public Light sun;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            escCanvas.enabled = !escCanvas.enabled;
+            scoreCanvas.enabled = !scoreCanvas.enabled;
+        }
+    }
+
+    public void ModifyLight(float vol)
+    {
+        sun.intensity = vol;
+    }
+}
